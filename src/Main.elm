7b091fae5c618b6819adcaa43665897e8fe2b250
Ben <rodforrb@mcmaster.ca>
2019-11-27 17:15:07 -0500

    bars added

--- src/Main.elm
+++ src/Main.elm
@@ -14,37 +14,46 @@ main =
         }
 
 init = 
-    { size = 5
+    { size = 20
     , minSize = 5
     , maxSize = 40
-    , notify = NotifyTap
+    , time = 0
     , currentButton = None
+    , adjLeft = -180
+    , adjRight = 160
+    , notify = NotifyTap
+    , array = [3, 8, 1, 5, 2, 0, 4, 7, 2, 5, 3, 8, 6, 1, 0, 6, 3, 8, 5, 3] -- random unsorted list
     }
 
 
 view model = 
     collage 600 400 <|
-        [ graphPaperCustom 10 1 (rgb 255 137 5) |> makeTransparent 0.25 -- axes and selected coordinate ticks
+        [ graphPaperCustom 10 1 (rgb 255 137 5) |> makeTransparent 0.1
         , group
-            [ rect 1000 0.5 |> filled brown
-            , rect 0.5 1000 |> filled brown
-            ]
-            |> move ( -120, 70 )
-
-        --, moveGraphicsX |> move ( 180, 220 )
-        --, moveGraphicsY |> move ( 60, 50 )
-        , group
-            [ rect 10 200 |> filled blue |> move (50, 50)
+            [
         --, triangle 10 |> filled (rgb 100 0 100) |> notifyTap SizePlus |> move ( 0, -5 ) |> notifyMouseDown (ButtonDown SizePlus) |> notifyMouseUp (ButtonDown None)
         --, triangle 10 |> filled (rgb 100 0 100) |> rotate (degrees 180) |> notifyTap SizeMinus |> move ( -110, -5 ) |> notifyMouseDown (ButtonDown SizeMinus) |> notifyMouseUp (ButtonDown None)
             ]
         , group
-            [ text ("More" ++ String.fromFloat model.uDilation) |> fixedwidth |> size 10 |> filled black |> move (-180, -10)
-            , text ("Fewer" ++ String.fromFloat model.uScale) |> fixedwidth |> size 10 |> filled black |> move (-180, -50)
-            --, text (String.fromInt model.size) |> fixedwidth |> size 10 |> filled black |> move (-210, -90)
+            [ text ("More") |> fixedwidth |> size 10 |> filled black |> move (model.adjRight, -50)
+            , text ("Fewer") |> fixedwidth |> size 10 |> filled black |> move (model.adjLeft, -50)
+            , text (String.fromFloat model.size) |> fixedwidth |> size 10 |> filled black
+                |> move ((model.adjLeft + (model.adjRight-model.adjLeft)*(model.size/(model.maxSize-model.minSize))) , -40)
             ]
+        , drawBars model.array 
         ]
 
+drawBars nums =
+    group (bars 0 nums [])
+
+-- n = counter
+-- list accumulates rectangles (bars)
+-- nums contains array of unsorted numbers
+bars n nums list =
+    case nums of
+        h::t ->
+            (rect 10 (10+20*h) |> filled blue |> move ((-160+15*n), -20+10*h)) :: (bars (n+1) t list)
+        [] -> []        
 
 
 type Msg m
@@ -54,6 +63,12 @@ type Msg m
     | MouseUp
     | ButtonDown ButtonDir
     | Notif Notifications
+
+type ButtonDir
+    = SizeMinus
+    | SizePlus
+    | Next
+    | None
 
 type Notifications
     = NotifyTap
@@ -72,18 +87,11 @@ type Notifications
     | NotifyTouchEnd
     | NotifyTouchEndAt
     | NotifyTouchMoveAt
-
-type ButtonDir
-    = SizeMinus
-    | SizePlus
-    | Next
-    | None
  
 update msg model =
     case msg of
         Tick t _ -> 
             let
-                -- we need to know how much time past since the last clock tick
                 timeChange = t - model.time
             in
                 { model
@@ -117,3 +125,33 @@ update msg model =
 
         MouseUp ->
             { model | currentButton = None }
+
+        Notif notif ->
+            { model | notify = notif }
+        
+        -- Next ->
+        --     { model
+        --         | array =
+                    
+        --         }
+
+-- mergeSort list =
+--     case list of
+--         [] -> []
+--         [_] -> list
+--         h::t ->
+--             (merge
+--                 (mergeSort (partition (\x -> x < ((length list)/2))))
+--             )
+
+merge list1 list2 =
+    case list1 of
+        [] -> list2
+        h1::t1 ->
+            case list2 of
+                [] -> []
+                h2::t2 ->
+                    if h1 < h2 then
+                        h1 :: merge t2 list2
+                    else    
+                        h2 :: merge list1 t2
